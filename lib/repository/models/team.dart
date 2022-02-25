d37ccc4184dc50eab612f7cd107eafa979046c71
Brydie Caine <brydie.caine1@gmail.com>
2022-02-25 09:01:57 +0000

    flesh out the Team model

--- lib/repository/models/team.dart
+++ lib/repository/models/team.dart
@@ -2,15 +2,26 @@
 class Team {
   const Team({
     required this.id,
+    required this.name,
+    this.crestUrl,
+    this.venue,
   });
 
   final int id;
+  final String name;
+  final String? crestUrl;
+  final String? venue;
 
   factory Team.fromJson(Map<String, dynamic>? json) {
     if (json == null) {
       throw const FormatException('null JSON provided to Team');
     }
 
-    return Team(id: json['id']);
+    return Team(
+      id: json['id'],
+      name: json['name'],
+      crestUrl: json['crestUrl'],
+      venue: json['venue'],
+    );
   }
 }
